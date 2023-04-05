ec5e0749494be79fadc9810a1cd8b334f068c1de
matin <matinhoseinzadeh@yahoo.com>
2023-04-05 10:32:51 +0330

    added Files

---
+++ one.js
@@ -0,0 +1,8 @@
+console.log("aaaaaaaaaaa");
+
+const aaa = {
+  test1:
+    "firsttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt",
+  test2:
+    "seeconddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+};
